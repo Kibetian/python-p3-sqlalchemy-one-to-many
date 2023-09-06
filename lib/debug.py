36c8c575144b845d840b2d65de39b8443a4b7ae5
Kibetian <126007714+Kibetian@users.noreply.github.com>
2023-09-06 11:46:08 +0300

    doneee

--- lib/debug.py
+++ lib/debug.py
@@ -10,4 +10,28 @@ if __name__ == '__main__':
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    import ipdb; ipdb.set_trace()
+    # import ipdb; ipdb.set_trace()
+
+# Access the first review instance in the database
+review = session.query(Review).first()
+review
+# => Review(id=1, score=7, game_id=1)
+
+# Get the game_id foreign key for the review instance
+review.game_id
+# => 1
+
+# Find a specific game instance using an ID
+session.query(Game).filter_by(id=review.game_id).first()
+# => Game(id=1, title=Jacob Floyd, platform=wii u)
+
+review.game
+# => Game(id=1, title=Jacob Floyd, platform=wii u)
+
+game = session.query(Game).first()
+game
+
+reviews = session.query(Review).filter_by(game_id=game.id)
+[review for review in reviews]
+
+game.reviews
